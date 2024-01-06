c3a0211128eafaeaa3eba7dced438fc87e24d49e
udonrm <b201395@hiroshima-u.ac.jp>
2024-01-06 13:11:17 +0900

    useStateを使って画像を設置

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
   return (
-    <div>
+    <>
       <header>
         <h1>Dogアプリ</h1>
       </header>
       <main>
         <p>犬の画像を表示するサイトです。</p>
-        <img
-          src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"
-          alt="犬の画像"
-        />
+        <img src={dogUrl} alt="犬の画像" />
       </main>
-    </div>
+    </>
   )
 }
